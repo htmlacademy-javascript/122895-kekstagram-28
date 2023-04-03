8e59e3b8af1b362b553e1f64ad84fb475f9f71db
Keks <keks@htmlacademy.ru>
2023-04-03 12:00:56 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/main.js
+++ 8/js/main.js
@@ -1,4 +1,8 @@
 import { createPhotos } from './setup.js';
 import { renderGallery } from './modal.js';
+import './form.js';
+import './validate.js';
+import './scale.js';
+import './effects.js';
 
 renderGallery(createPhotos());
